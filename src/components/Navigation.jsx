a22cf426fb56a00b11ee910cfa6b20b6bcdb1b67
Yurika Jacobsson <nakiruy@gmail.com>
2018-01-16 20:13:40 -0800

    Add navigation changes with style.

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div className="navbar">
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">
+            <div id="logo">YJ</div>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="">WORK</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>ABOUT</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://drive.google.com/file/d/1BFVRhbwHr4rAcKRh8kM8uteZXV3wg5Qv/">RESUME</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Navigation
