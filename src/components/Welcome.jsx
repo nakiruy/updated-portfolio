d1b80286d85bb9a0f9fcdde46eaa5f42947bef58
Yurika Jacobsson <nakiruy@gmail.com>
2018-05-15 13:05:44 -0700

    Add file for potential chat interface.

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// put chat things here
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="welcome">
+      </div>
+    )
+  }
+}
+
+export default Welcome
