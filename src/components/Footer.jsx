52fb2897091c5c69b197b02c6365fe73acf55db6
Yurika Jacobsson <nakiruy@gmail.com>
2018-06-21 16:45:29 -0700

    Replace social media icons.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Navbar } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+
+import email from '../assets/email.png';
+import linkedin from '../assets/linkedin.png';
+import instagram from '../assets/instagram.png';
 
 class Footer extends React.Component {
   constructor(props) {
@@ -13,7 +17,24 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-          <div class="icons">INSERT ICONS HERE github ig</div>
+          <div class="icons">
+            <Container>
+              <Row>
+                <Col sm="4">
+                  <a href="mailto:nakiruy@gmail.com">
+                      <img className="contact-icon" src={email} alt="Email" />
+                  </a></Col>
+                  <Col sm="4">
+                  <a href="https://www.linkedin.com/in/yurikajacobsson/">
+                      <img className="contact-icon" src={linkedin} alt="LinkedIn" />
+                  </a></Col>
+                  <Col sm="4">
+                  <a href="https://www.instagram.com/yurikaffeine/">
+                      <img className="contact-icon" src={instagram} alt="Instagram" />
+                  </a></Col>
+              </Row>
+            </Container>
+          </div>
           <div id="copyright">copyright © 2018 yurika jacobsson. all rights reserved.</div>
       </div>
     );
