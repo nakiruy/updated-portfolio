03d7b67d6897c52f3012782bc38e4490bdff0696
Yurika Jacobsson <nakiruy@gmail.com>
2018-12-17 21:44:20 -0800

    Save greeting and navigation changes.

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
 
 import Contact from './Contact';
@@ -18,8 +19,12 @@ class Greetings extends React.Component {
       <div>
         <Container>
           <Col className="greetings">
-            {/* <h3 id="name">Hello. I'm Yurika.</h3>
-            <h3 id="headline">a designer and project manager based in the bay</h3> */}
+            <h1 id="headline">Hi, I'm <Link exact to="/about"><span id="name">Yurika</span></Link>.</h1>
+            <h6 id="subtitle">A designer and a project manager based in the bay</h6> 
+            {/* <div className="arrowDownContainer">
+              <span className="arrowDown" id="arrowOne"></span>
+              <span className="arrowDown" id="arrowTwo"></span>
+            </div> */}
           </Col>
         </Container>
       </div>
