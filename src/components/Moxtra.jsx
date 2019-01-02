93be4a6556bb7e804e25bfe92c7b5709e876c99f
Yurika Jacobsson <nakiruy@gmail.com>
2019-01-02 11:42:35 -0800

    Add button animation.

--- src/components/Moxtra.jsx
+++ src/components/Moxtra.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
 
 const Moxtra = (props) => {
   return (
     <div>
       <Jumbotron className="moxtra">
-        <h1 className="display-3">Hello, world!</h1>
+        <h1 className="display-3">Moxtra Mobile Onboarding</h1>
         <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
         <p className="lead">
-          <Button color="secondary">View Project</Button>
+          <a href="#" className="btn">View Project</a>
         </p>
       </Jumbotron>
     </div>
--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
 
 const Spotify = (props) => {
   return (
     <div>
       <Jumbotron className="spotify">
-        <h1 className="display-3">Hello, world!</h1>
+        <h1 className="display-3">Spotify Driving Mode</h1>
         <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
         <p className="lead">
-          <Button color="secondary">View Project</Button>
+          <a href="#" className="btn">View Project</a>
         </p>
       </Jumbotron>
     </div>
