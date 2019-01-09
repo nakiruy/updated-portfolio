8619e309c7475b54bd2ee9dc056bd877c68dea2b
Yurika Jacobsson <nakiruy@gmail.com>
2019-01-09 14:39:42 -0800

    Remove unnecessary href and typo.

--- src/components/MoxtraJumbo.jsx
+++ src/components/MoxtraJumbo.jsx
@@ -9,7 +9,7 @@ const MoxtraJumbo = (props) => {
         <h1 className="display-3" id="moxtra-lead"><span className="highlight">Onboarding Process for Moxtra</span></h1>
         <p className="lead" id="moxtra-lead"><span className="highlight">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</span></p>
         <p className="lead" id="moxtra-lead">
-          <Link exact to="/moxtra"><a href="#" className="btn">View Project</a></Link>
+          <Link exact to="/moxtra"><a className="btn">View Project</a></Link>
         </p>
       </Jumbotron>
     </div>
