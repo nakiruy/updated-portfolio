6110a4d4403289483b47b04cb9393865474072ac
Yurika Jacobsson <nakiruy@gmail.com>
2019-01-10 16:05:55 -0800

    Add specific style for mobile projects view.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,7 +18,7 @@ class Contact extends React.Component {
         <Container className="contact">
           <Col className="contact-method">
             <hr></hr>
-            <h1 className="footer"><span id="name">Get in touch.</span></h1>
+            <h1 className="footer">Interested in working together? <span id="getintouch">Get in touch.</span></h1>
           </Col>
         </Container>
       </div>
