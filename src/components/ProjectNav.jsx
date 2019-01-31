744a75633e2ab0fa033a2cb2db55e89d8b64da06
Yurika Jacobsson <nakiruy@gmail.com>
2019-01-31 14:42:13 -0800

    Add functionality of dynamic project buttons based on path.

--- src/components/ProjectNav.jsx
+++ src/components/ProjectNav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 import Navigation from './Navigation';
 import Footer from './Footer';
@@ -14,11 +15,37 @@ class ProjectNav extends React.Component {
   }
 
   render() {
+    let prevButton;
+    let nextButton;
+
+    if (window.location.pathname === "/projects/tapcast") {
+      prevButton = <p className="lead" id="moxtra-lead"><Link exact to="/projects/moxtra"><span className="highlight-moxtra" id="btn-moxtra"><a className="btn">View Project</a></span></Link></p>
+      nextButton = <p className="lead" id="spotify-lead"><Link exact to="/projects/spotify"><span className="highlight" id="btn-spotify"><a className="btn">View Project</a></span></Link></p>
+    } else if (window.location.pathname === "/projects/moxtra") {
+      prevButton = <p className="lead" id="tapcast-lead"><Link exact to="/projects/tapcast"><span className="highlight" id="btn-tapcast"><a className="btn">View Project</a></span></Link></p>
+      nextButton = <p className="lead" id="spotify-lead"><Link exact to="/projects/spotify"><span className="highlight" id="btn-spotify"><a className="btn">View Project</a></span></Link></p>
+    } else {
+      prevButton = <p className="lead" id="tapcast-lead"><Link exact to="/projects/tapcast"><span className="highlight" id="btn-tapcast"><a className="btn">View Project</a></span></Link></p>
+      nextButton = <p className="lead" id="moxtra-lead"><Link exact to="/projects/moxtra"><span className="highlight-moxtra" id="btn-moxtra"><a className="btn">View Project</a></span></Link></p>
+    }
+
     return (
-    <Container className="project-nav" fluid>
-      See Next
-      See Previous
-    </Container>
+      <Row className="project-nav">
+      <Col md="6">
+        <Card body>
+          <CardTitle>See Prev</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          {prevButton}
+        </Card>
+      </Col>
+      <Col md="6">
+        <Card body>
+          <CardTitle>See Next</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          {nextButton}
+        </Card>
+      </Col>
+    </Row>
     )
   }
 }
