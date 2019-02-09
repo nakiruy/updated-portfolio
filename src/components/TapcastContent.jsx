96b68da302d1a5e6a89976033a645c41806c04b3
Yurika Jacobsson <nakiruy@gmail.com>
2019-02-08 18:15:37 -0800

    Add individual proj pics.

--- src/components/TapcastContent.jsx
+++ src/components/TapcastContent.jsx
@@ -28,23 +28,24 @@ const TapcastContent = () => {
       </CardGroup>
       <Col className="tapcast-process">
         <hr></hr>
-        <h6 className="secondary-header tapcast-color" id="top-header">01. The Challenge</h6>
+        <h6 className="secondary-header tapcast-color">01. The Beginning</h6>
+        <h6 className="secondary-header tapcast-color" id="top-header"><span className="bold">02.</span> The Challenge</h6>
         <Media body>
           Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
         </Media>
-        <h6 className="secondary-header tapcast-color">02. The Research</h6>
+        <h6 className="secondary-header tapcast-color">03. The Research</h6>
         <Media body>
           Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
         </Media>
-        <h6 className="secondary-header tapcast-color">03. The Process</h6>
+        <h6 className="secondary-header tapcast-color">04. The Process</h6>
         <Media body>
           Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
         </Media>
-        <h6 className="secondary-header tapcast-color">04. The Solution</h6>
+        <h6 className="secondary-header tapcast-color">05. The Solution</h6>
         <Media body>
           Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
         </Media>
-        <h6 className="secondary-header tapcast-color">05. The End</h6>
+        <h6 className="secondary-header tapcast-color"><span className="bold">06.</span> The End</h6>
         <Media body>
           Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
         </Media>
