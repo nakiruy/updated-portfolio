695ce31fd88425e708e572291311c0fa95f81a81
Yurika Jacobsson <nakiruy@gmail.com>
2019-03-07 15:39:47 -0800

    Change jumbo copy and contact banner image.

--- src/components/TapcastJumbo.jsx
+++ src/components/TapcastJumbo.jsx
@@ -7,7 +7,7 @@ const TapcastJumbo = (props) => {
       <Jumbotron className="tapcast-jumbo">
         <Card className="description-wrapper" body>
           <h1 className="display-3" id="tapcast-lead"><span className="highlight">Redesigning Tapcast Platform</span></h1>
-          <p className="lead" id="tapcast-lead"><span className="highlight">Marketing intelligence, data analytic platform.</span></p>
+          <p className="lead" id="tapcast-lead"><span className="highlight">Marketing intelligence platform for Facebook advertising.</span></p>
           <p className="lead" id="tapcast-lead">
             <Link exact to="/projects/tapcast"><span className="highlight" id="btn-tapcast"><a className="btn">View Project</a></span></Link>
           </p>
