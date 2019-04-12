5114767c821566a48084a828a0aecae2d87c71b4
Yurika Jacobsson <nakiruy@gmail.com>
2019-04-12 14:51:17 -0700

    Remove loading screen.

--- src/App.js
+++ src/App.js
@@ -10,27 +10,27 @@ import AllProjects from './components/AllProjects';
 import ScrollUpButton from './components/ScrollUpButton';
 import Footer from './components/Footer';
 
-import LoadingScreen from './components/LoadingScreen';
+// import LoadingScreen from './components/LoadingScreen';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      loading: true
+      // loading: true
     };
   }
   componentDidMount(){
-    setTimeout(() => this.setState({ loading : false }), 1500);
+    // setTimeout(() => this.setState({ loading : false }), 1500);
     AOS.init({
       duration : 2000
     })
     window.scrollTo(0, 0);
   }
   render() {
-    const { loading } = this.state;
-    if(loading) {
-      return <LoadingScreen />
-    }
+    // const { loading } = this.state;
+    // if(loading) {
+    //   return <LoadingScreen />
+    // }
     return (
       <div>
         <Container className="header" fluid>
