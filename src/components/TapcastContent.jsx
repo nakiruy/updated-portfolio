450fc16c928bfc03d441a3874692eaae5fbeccd9
Yurika Jacobsson <nakiruy@gmail.com>
2019-04-15 16:33:27 -0700

    Add ability to zoom images on click and add to T.

--- src/components/TapcastContent.jsx
+++ src/components/TapcastContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Media, Col, Row, Jumbotron, Container, Card, CardImg, CardTitle, CardText, CardGroup, CardBody } from 'reactstrap';
 import AOS from 'aos';
+import ImageZoom from 'react-medium-image-zoom';
 
 class TapcastContent extends React.Component {
   constructor(props) {
@@ -59,12 +60,34 @@ class TapcastContent extends React.Component {
               </Col>
               <Col lg="8" data-aos="fade-up">
                 <Media body>
-                  <span className="chunky-underline" id="tapcast-underline-color">Tapcast platform is complicated.</span> <br></br><br></br>
-                  <CardImg top width="100%" src="http://assets.yurika.design/tapcast/oldtapcast.png" className="proj-img"></CardImg> <br></br>
+                  <span className="chunky-underline" id="tapcast-underline-color">Tapcast platform is complicated.</span> <br></br><br></br>                  
+                  <ImageZoom
+                    image={{
+                      src: 'http://assets.yurika.design/tapcast/oldtapcast.png',
+                      alt: 'Old Tapcast',
+                      className: 'img',
+                      style: { width: '100%' }
+                    }}
+                    zoomImage={{
+                      src: 'http://assets.yurika.design/tapcast/oldtapcast.png',
+                      alt: 'Old Tapcast',
+                    }}
+                  /><br></br>
                   <span className="img-caption">tapcast v2.0</span> <br></br><br></br>
                   Why? The purpose of this platform is to provide marketing insights and help demystify <a href="https://www.facebook.com/business/insights/people/audiences"><span className="tapcast-links">Facebook audiences</span></a>. However, advertising on Facebook is confusing to many (read <a href="https://www.wired.com/story/facebooks-targeted-ads-are-more-complex-than-it-lets-on/"><span className="tapcast-links">here</span></a>, <a href="https://www.postplanner.com/blog/how-to-fix-facebook-ads-not-working"><span className="tapcast-links">here</span></a>, and <a href="https://www.929marketing.com/blog/this-is-why-your-facebook-ads-didnt-work"><span className="tapcast-links">here</span></a>) and without a baseline of knowledge of advertising on Facebook, it is a challenge to understand the platform, let alone its merit.<br></br><br></br>
                   In addition, the new platform is vastly different from before. The previous iteration of the platform focused on targeted videos (think <a href="https://www.intercom.com"><span className="tapcast-links">Intercom</span></a> but instead of chat, a video) which allowed users to install a video widget to their websites through Tapcast SDK. <br></br><br></br>
-                  <CardImg top width="100%" src="http://assets.yurika.design/tapcast/tapcastPIP.png" className="proj-img"></CardImg> <br></br>
+                  <ImageZoom
+                    image={{
+                      src: 'http://assets.yurika.design/tapcast/tapcastPIP.png',
+                      alt: 'Tapcast PIP',
+                      className: 'img',
+                      style: { width: '100%' }
+                    }}
+                    zoomImage={{
+                      src: 'http://assets.yurika.design/tapcast/tapcastPIP.png',
+                      alt: 'Tapcast PIP',
+                    }}
+                  /><br></br>
                   <span className="img-caption">Example of Tapcast SDK </span> <br></br><br></br>
                   Unfortunately, this meant that many of our existing users did not have the prior knowledge of Facebook advertising and <span className="chunky-underline" id="tapcast-underline-color">we quickly found out that understanding the new platform was a huge struggle for many</span>. As expected, we lost some existing users along the way and soon after, we started observing low conversion rates for new users as well.<br></br><br></br>
                   In my view, a redesign was a no-brainer and I felt strongly that it would be <span className="chunky-underline" id="tapcast-underline-color">the strategic ingredient to increase conversion rates</span>.
@@ -81,7 +104,18 @@ class TapcastContent extends React.Component {
                   After comparing and contrasting a handful of companies as well as coupling user sessions/gathering feedbacks from our customer success team, it was clear that <span className="chunky-underline" id="tapcast-underline-color">our onboarding wasn't enough</span>.
                   We provided no clear path on how to utilize the platform with zero cues to prompt users to act.<br></br><br></br> 
                   Moreover, we concluded internally that Tapcast as a product requires a heavy ramp-up period and extensive onboarding. Thus, keeping in mind the high priority business goal of converting as many new users as possible, I decided <span className="chunky-underline" id="tapcast-underline-color">to focus on the new user journey and payment flow</span> in this redesign.<br></br><br></br> 
-                  <CardImg top width="100%" src="http://assets.yurika.design/tapcast/tapcastFlow.png" className="proj-img no-shadow"></CardImg> <br></br>
+                  <ImageZoom
+                    image={{
+                      src: 'http://assets.yurika.design/tapcast/tapcastFlow.png',
+                      alt: 'Tapcast Flowchart',
+                      className: 'img',
+                      style: { width: '100%' }
+                    }}
+                    zoomImage={{
+                      src: 'http://assets.yurika.design/tapcast/tapcastFlow.png',
+                      alt: 'Tapcast Flowchart',
+                    }}
+                  /><br></br>
                   <span className="img-caption">Signup to payment flowchart</span> <br></br><br></br>
                   To be competitive, I also felt that our platform needed some serious rebranding. I aimed for a modern, clean, simple look while being respectful of the previous design. I first redesigned the logo:<br></br><br></br>
                   <Row>
@@ -96,10 +130,32 @@ class TapcastContent extends React.Component {
                   With logo design complete, I started to iteratively build the final design.<br></br><br></br>
                   <Row>
                     <Col md="6">
-                      <CardImg top width="100%" src="http://assets.yurika.design/tapcast/mockupBW.png" className="proj-img"></CardImg>
+                      <ImageZoom
+                        image={{
+                          src: 'http://assets.yurika.design/tapcast/mockupBW.png',
+                          alt: 'Tapcast Mockup BW',
+                          className: 'img',
+                          style: { width: '100%' }
+                        }}
+                        zoomImage={{
+                          src: 'http://assets.yurika.design/tapcast/mockupBW.png',
+                          alt: 'Tapcast Mockup BW',
+                        }}
+                      /><br></br>
                     </Col>
                     <Col md="6">
-                      <CardImg top width="100%" src="http://assets.yurika.design/tapcast/mockupColor.png" className="proj-img"></CardImg>
+                      <ImageZoom
+                        image={{
+                          src: 'http://assets.yurika.design/tapcast/mockupColor.png',
+                          alt: 'Tapcast Mockup Color',
+                          className: 'img',
+                          style: { width: '100%' }
+                        }}
+                        zoomImage={{
+                          src: 'http://assets.yurika.design/tapcast/mockupColor.png',
+                          alt: 'Tapcast Mockup Color',
+                        }}
+                      /><br></br>
                     </Col>
                   </Row>
                   <span className="img-caption">Mockup iterations</span><br></br><br></br>
@@ -113,15 +169,70 @@ class TapcastContent extends React.Component {
               <Col lg="8" data-aos="fade-up">
                 <Media body>
                   <a href="https://invis.io/7FQVFPD92ZM"><span className="tapcast-links">Click here to view the full prototype</span></a><br></br><br></br>
-                  <CardImg top width="100%" src="http://assets.yurika.design/tapcast/tapcast-platform.png" className="proj-img"></CardImg> <br></br>
+                  <ImageZoom
+                      image={{
+                        src: 'http://assets.yurika.design/tapcast/tapcast-platform.png',
+                        alt: 'Dashboard',
+                        className: 'img',
+                        style: { width: '100%' }
+                      }}
+                      zoomImage={{
+                        src: 'http://assets.yurika.design/tapcast/tapcast-platform.png',
+                        alt: 'Dashboard',
+                      }}
+                  /><br></br>
                   <span className="img-caption">dashboard</span> <br></br><br></br>
-                  <CardImg top width="100%" src="http://assets.yurika.design/tapcast/tapcast-landing.png"className="proj-img"></CardImg> <br></br>
+                  <ImageZoom
+                      image={{
+                        src: 'http://assets.yurika.design/tapcast/tapcast-landing.png',
+                        alt: 'Landing Page',
+                        className: 'img',
+                        style: { width: '100%' }
+                      }}
+                      zoomImage={{
+                        src: 'http://assets.yurika.design/tapcast/tapcast-landing.png',
+                        alt: 'Landing Page',
+                      }}
+                  /><br></br>
                   <span className="img-caption">landing page</span> <br></br><br></br>
-                  <CardImg top width="100%" src="http://assets.yurika.design/tapcast/tapcast-signup.png" className="proj-img"></CardImg> <br></br>
+                  <ImageZoom
+                      image={{
+                        src: 'http://assets.yurika.design/tapcast/tapcast-signup.png',
+                        alt: 'Signup Page',
+                        className: 'img',
+                        style: { width: '100%' }
+                      }}
+                      zoomImage={{
+                        src: 'http://assets.yurika.design/tapcast/tapcast-signup.png',
+                        alt: 'Signup Page',
+                      }}
+                  /><br></br>
                   <span className="img-caption">sign up</span> <br></br><br></br>
-                  <CardImg top width="100%" src="http://assets.yurika.design/tapcast/tapcast-audience.png" className="proj-img"></CardImg> <br></br>
+                  <ImageZoom
+                      image={{
+                        src: 'http://assets.yurika.design/tapcast/tapcast-audience.png',
+                        alt: 'Audience Page',
+                        className: 'img',
+                        style: { width: '100%' }
+                      }}
+                      zoomImage={{
+                        src: 'http://assets.yurika.design/tapcast/tapcast-audience.png',
+                        alt: 'Audience Page',
+                      }}
+                  /><br></br>
                   <span className="img-caption">audience page</span> <br></br><br></br>
-                  <CardImg top width="100%" src="http://assets.yurika.design/tapcast/tapcast-payment.png" className="proj-img"></CardImg> <br></br>
+                  <ImageZoom
+                      image={{
+                        src: 'http://assets.yurika.design/tapcast/tapcast-payment.png',
+                        alt: 'Payment Page',
+                        className: 'img',
+                        style: { width: '100%' }
+                      }}
+                      zoomImage={{
+                        src: 'http://assets.yurika.design/tapcast/tapcast-payment.png',
+                        alt: 'Payment Page',
+                      }}
+                  /><br></br>
                   <span className="img-caption">payment</span> <br></br>
                 </Media>
               </Col>
