927256434a5e03b249cdcf8ef8d0b4aea47abf83
Yurika Jacobsson <nakiruy@gmail.com>
2019-04-23 11:51:22 -0700

    Add another condition for the See you soon tab change and add more content to the cover letter page.

--- src/components/CoverLetter.jsx
+++ src/components/CoverLetter.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-
-import { Container } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Container, Col } from 'reactstrap';
+import AOS from 'aos';
 
 import Navigation from './Navigation';
 import Footer from './Footer';
@@ -15,14 +14,27 @@ class CoverLetter extends React.Component {
   }
   componentDidMount(){
     window.scrollTo(0, 0);
+    AOS.init({
+      duration : 2000
+    })
   }
 
   render() {
     return (
       <div>
-      <Container className="not-found">
-        <Navigation />
-        <h1 className="headline" id="not-header">Cover Letter<span id="period">.</span></h1>
+      <Navigation />
+      <Container>
+        <Col md="9" className="greetings">
+          <h1 className="headline" data-aos="fade-up">Welcome to my interactive cover letter, <span id="cover-letter-name">[NAME]</span>.</h1>
+          <div data-aos="fade-up">
+            <h6 className="secondary-header" data-aos="fade-up">Why hire me?</h6>
+            <h6 className="about-desc" id="cover-letter-desc">
+              Aside from her experience and skills, 
+            </h6>
+          </div>
+          <h6 className="secondary-header">testimonials</h6>
+          <h1 className="headline" id="not-header">Thank you for stopping by<span id="period">.</span></h1>
+        </Col>
       </Container>
       <Container className="footer" fluid>
         <Footer />
