53c436d5d77de738315f295c3737eb36a259d573
Yurika Jacobsson <nakiruy@gmail.com>
2019-06-20 15:44:25 -0700

    Solidify groupon critique content.

--- src/components/GrouponReviewContent.jsx
+++ src/components/GrouponReviewContent.jsx
@@ -28,7 +28,15 @@ class GrouponReviewContent extends React.Component {
             <Col lg="8" data-aos="fade-up">
               <Media body>
                 <a href="https://groupon.com/"><span className="groupon-links">Groupon</span></a> is an online marketplace that connects people with local businesses by offering discounts in activities, travel, goods, services, and more. As they put it, their mission is <span className="chunky-underline" id="groupon-underline-color">to be the daily habit in local commerce.</span><br></br><br></br> 
-                According to their <a href="https://s22.q4cdn.com/731250486/files/doc_downloads/2019/Q1-2019-Fact-Sheet_Final.pdf"><span className="groupon-links">Q1 2019 Fact Sheet</span></a>, they currently have 47.2 million active users with 20% of user traffic stemming from desktop. Their primary users are comprised of college-educated, urban women ranging from ages 25 to 44 and secondary users being early adopters with a sizeable disposable income (<a href="https://www.feedough.com/how-does-groupon-make-money-groupon-business-model/"><span className="groupon-links">source</span></a>). As business, Groupon generates revenue through commissions for every customers referred to merchant.<br></br><br></br>
+                According to their <a href="https://s22.q4cdn.com/731250486/files/doc_downloads/2019/Q1-2019-Fact-Sheet_Final.pdf"><span className="groupon-links">Q1 2019 Fact Sheet</span></a>...<br></br><br></br>
+                <ul className="takeaways" id="groupon-consideration">
+                    <li>47.2 million active users</li>
+                    <li>20% of user traffic from desktop</li>
+                    <li>Main demographic are college-educated, urban women ranging from ages 25 to 44 (<a href="https://www.feedough.com/how-does-groupon-make-money-groupon-business-model/"><span className="groupon-links">source</span></a></li>
+                    <li>Primary users - subscribers using Groupon to purchase discounted goods, services, etc.</li>
+                    <li>Secondary users - local businesses listing goods, services, etc. on the website</li>
+                    <li>Generates revenue through commissions for every customers referred to merchant</li>
+                  </ul><br></br><br></br>
               </Media>
             </Col>
           </Row>
@@ -38,7 +46,6 @@ class GrouponReviewContent extends React.Component {
             </Col>
             <Col lg="8" data-aos="fade-up">
               <Media body>
-                <Media body>
                   <ul className="takeaways" id="groupon-consideration">
                     <li><b>Usability.</b> Is it easy to use? </li>
                     <li><b>Clarity.</b> Is it clear?</li>
@@ -46,7 +53,6 @@ class GrouponReviewContent extends React.Component {
                     <li><b>Effectiveness.</b> Is it solving user problems efficiently?</li>
                     <li><b>Feedback.</b> Is it communicating successes and/or failures to its users?</li>
                   </ul><br></br><br></br>
-                </Media>
               </Media>
             </Col>
           </Row>
@@ -126,6 +132,27 @@ class GrouponReviewContent extends React.Component {
                   }}
                   zoomImage={{
                     src: 'http://assets.yurika.design/groupon-ui.png',
+                    alt: 'Groupon UI',
+                  }}
+                /><br></br>
+              </Media>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg="4" data-aos="fade-up">
+              <h6 className="secondary-header process-heading"><div className="numbers groupon-color"> </div>Accessibility</h6>
+            </Col>
+            <Col lg="8" data-aos="fade-up">
+              <Media body>
+                <ImageZoom
+                  image={{
+                    src: 'http://assets.yurika.design/groupon-wcag-rating.png',
+                    alt: 'Groupon UI',
+                    className: 'proj-img no-shadow',
+                    style: { width: '100%' }
+                  }}
+                  zoomImage={{
+                    src: 'http://assets.yurika.design/groupon-wcag-rating.png',
                     alt: 'Groupon UI',
                   }}
                 /><br></br>
