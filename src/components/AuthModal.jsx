e401c816bef05775cb5537a8afd8a6e27ff9906e
Yurika Jacobsson <nakiruy@gmail.com>
2019-07-23 17:32:50 -0700

    AuthModal now redirecting or not based on the input of correct pw.

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Input } from 'reactstrap';
 
 class AuthModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: props.initialModalState
+      modal: props.initialModalState,
+      password: '',
+      redirect: false
     };
   }
 
   handlePasswordChange = (e) =>  {
-    this.setState({password: e.target.value});
+    e.preventDefault()
+    this.setState({ password: e.target.value });
   }
 
   handleSubmit = () => {
-    console.log('submit');
+    if (this.state.password === 'Hello') {
+      console.log(this.state.password, 'submit')
+      this.setState({ redirect: true })
+    } 
   }
 
   render() {
+    const { redirect } = this.state
     return (
       <div className="auth-modal">
         {/* <Button color="danger" onClick={this.toggle}>TOGGLE</Button> */}
@@ -26,11 +34,16 @@ class AuthModal extends React.Component {
           <ModalBody>
             <FormGroup>
               <Label for="project-password">To view, please enter the password or <a href="mailto:nakiruy@gmail.com"><span id="request-pw">request password</span></a>.</Label>
-              <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+              <Input type="password" name="password" id="examplePassword" placeholder="Password" onChange={this.handlePasswordChange} />
             </FormGroup>
           </ModalBody>
           <ModalFooter>
-            <span className="btn" onClick={this.handleSubmit}>View Project</span>
+            <span className="btn" onClick={this.handleSubmit}>
+              {redirect && (
+                <Redirect push to={ null || '/projects/tapcastbilling' } />
+              )}
+              View Project
+            </span>
           </ModalFooter>
         </Modal>
       </div>
