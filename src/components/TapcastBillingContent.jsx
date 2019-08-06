40576761291b219d6439aa376001cadaeff565fb
Yurika Jacobsson <nakiruy@gmail.com>
2019-08-06 15:22:26 -0700

    Add highlights and links to the billing proj.

--- src/components/TapcastBillingContent.jsx
+++ src/components/TapcastBillingContent.jsx
@@ -50,7 +50,7 @@ class TapcastBillingContent extends React.Component {
               <Media body>
                 <a href="https://www.tapcast.io"><span className="tapcast-billing-links">Tapcast</span></a> is a data-driven, marketing intelligence platform aimed to help marketers to understand and scale their Facebook audiences. <br></br><br></br>
                 Before Q4 2018, we frequently changed billing plans (in fact, we audited internally and found that we changed our billing model six times since the beginning of the year) and desperately needed a more permanent pricing model for our SaaS platform. <br></br><br></br>
-                In addition to the ever-changing landscape of Facebook after the Cambridge Analytica scandal, we were faced with a challenge of coming up with a billing model that satisfied Facebook, as well as our internal teams and customers. As a designer and a PM at Tapcast, I established the user flow, designed mockups, and managed this project from inception to deployment. <br></br>             
+                In addition to the ever-changing landscape of Facebook after the <a href="https://en.wikipedia.org/wiki/Facebook%E2%80%93Cambridge_Analytica_data_scandal"><span className="tapcast-billing-links">Cambridge Analytica scandal</span></a>, we were faced with <span className="chunky-underline" id="tapcast-billing-underline-color">a challenge of coming up with a billing model that satisfied Facebook, as well as our internal teams and customers.</span> As a designer and a PM at Tapcast, I established the user flow, designed mockups, and managed this project from inception to deployment. <br></br>             
               </Media>
             </Col>
             </Row>
@@ -60,7 +60,7 @@ class TapcastBillingContent extends React.Component {
               </Col>
               <Col lg="8" data-aos="fade-up">
                 <Media body>
-                  One of the biggest problems was born out of our own doing. As briefly mentioned above, we had implemented six different pricing models within the year. These changes ranged from the number of ad accounts to subscription pricing and from plan names to whether or not we should charge clients based on Facebook ad spend. As changes accumulated, it was becoming increasingly difficult to keep up with them. We were spending a lot of time keeping track of legacy billing models which led to a massive workload debt in operations and development sides of the business.  <br></br><br></br>
+                  <span className="chunky-underline" id="tapcast-billing-underline-color">One of our biggest problems was born out of our own doing.</span> As briefly mentioned above, we had implemented <span className="chunky-underline" id="tapcast-billing-underline-color">six different pricing models</span> within the year. These changes ranged from the number of ad accounts to subscription pricing and from plan names to whether or not we should charge clients based on Facebook ad spend. As changes accumulated, it was becoming increasingly difficult to keep up with them. We were spending a lot of time keeping track of legacy billing models which led to a massive workload debt in operations and development sides of the business.  <br></br><br></br>
                   Moreover, despite the efforts and pivots, we discovered via funnel analysis that our total conversion rate of purchases was less than ideal.<br></br><br></br>
                   <ImageZoom
                         image={{
@@ -76,7 +76,7 @@ class TapcastBillingContent extends React.Component {
                         }}
                     /><br></br>
                     <span className="img-caption">Initial purchase conversion rate</span> <br></br><br></br>
-                  Needless to say, we strongly felt that we could do better. 
+                    <span className="chunky-underline" id="tapcast-billing-underline-color">Needless to say, we felt strongly that we could do better.</span>
                 </Media>
               </Col>
             </Row>
@@ -106,9 +106,9 @@ class TapcastBillingContent extends React.Component {
                   <li>Then the user would have to select a plan, enter a billing address, and enter a credit card number to complete process</li>
                 </ul>
                 <br></br>
-                Comparing and contrasting what we had versus customer complaints, we gained a high-level sense of where we went south. However, we wanted a clearer picture of the problem. We all agreed to diverge and conduct an analysis of our own. As a part of this process, I conducted quick surveys both internally and externally and observed customer behaviors using <a href="https://www.fullstory.com/"><span className="tapcast-billing-links">FullStory</span></a>.
+                <span className="chunky-underline" id="tapcast-billing-underline-color">Comparing and contrasting what we had versus customer complaints, we gained a high-level sense of where we went south.</span> However, we wanted a clearer picture of the problem. We all agreed to diverge and conduct an analysis of our own. As a part of this process, I conducted quick surveys both internally and externally and observed customer behaviors using <a href="https://www.fullstory.com/"><span className="tapcast-billing-links">FullStory</span></a>.
                 <br></br><br></br>
-                After numerous planned and ad-hoc meetings in regards to this problem, I combined our findings and broke down what changes we needed to implement for a new billing process on Asana: 
+                After numerous planned and ad-hoc meetings in regards to this problem, I combined our findings and broke down what changes we needed to implement for a new billing process on <a href="https://asana.com/"><span className="tapcast-billing-links">Asana</span></a>: 
                 <br></br><br></br>
                 <ul className="tapcast-painpoints" id="tapcast-solution">
                   <li>Create a dynamic pricing model based on the client’s trailing 30-day ad spend on Facebook and the number of ad accounts</li>
@@ -209,16 +209,16 @@ class TapcastBillingContent extends React.Component {
                         }}
                     /><br></br>
                     <span className="img-caption">Enterprise plan</span> <br></br><br></br>
-                    Key changes to note:
+                    Key changes to note -
                     <br></br>
                     <ul className="tapcast-painpoints" id="tapcast-solution">
-                      <li>Increased pricing visibility to users by combining the selection of ad accounts with the payment/upgrade functionality (part of onboarding)</li>
+                      <li><span className="chunky-underline" id="tapcast-billing-underline-color">Increased pricing visibility</span> to users by combining the selection of ad accounts with the payment/upgrade functionality (part of onboarding)</li>
                       <li>Previously, a user could upgrade their plan from /subscription/upgrade path and select their desired monthly plan</li>
-                      <li>Now, users can pay upfront if they’d like as payment modal is part of the onboarding and even if they choose not to, they’ll know exactly where to go at a later time</li>
+                      <li>Now, <span className="chunky-underline" id="tapcast-billing-underline-color">users can pay upfront</span> if they’d like as payment modal is part of the onboarding and even if they choose not to, they’ll know exactly where to go at a later time</li>
                       <li>Added a high-level overview of the number of ad accounts, 30 days associated ad spend, and monthly price</li>
                       <li>Added a breakdown tooltip if a user would like to see the granular calculation of given price</li>
                       <li>Incorporated a summary for each payment scenario so the customer can see the terms of billing in full transparency</li>
-                      <li>Decreased friction by minimizing the steps in the payment process (previously, we had four steps and now two)</li>
+                      <li><span className="chunky-underline" id="tapcast-billing-underline-color">Decreased friction</span> by minimizing the steps in the payment process (previously, we had four steps and now two)</li>
                     </ul>
                 </Media>
               </Col>
@@ -229,7 +229,7 @@ class TapcastBillingContent extends React.Component {
               </Col>
               <Col lg="8" data-aos="fade-up">
                 <Media body>
-                We successfully released this particular pricing iteration back in early December 2018 as part of our v2.5.0 release. Since then, we have doubled the conversion rate from user registration to purchase (from 3.8% to 8.2%) and the volume of related support tickets decreased significantly (previously, most of our support tickets related to pricing was in regards to how to upgrade and why pricing was the way it was - we have now tackled that). Internally, we were able to decrease our workload debt not only in customer service but in operations, development, and sales since we no longer needed to keep track of six different legacy pricing plans. We were then able to contribute our time in more high-value tasks. As we are a small team, this was a huge win for us. 
+                We successfully released this particular pricing iteration back in early December 2018 as part of our v2.5.0 release. Since then, <span className="chunky-underline" id="tapcast-billing-underline-color">we have doubled the conversion rate</span> from user registration to purchase (from 3.8% to 8.2%) and the volume of related support tickets <span className="chunky-underline" id="tapcast-billing-underline-color">decreased significantly</span> (previously, most of our support tickets related to pricing was in regards to how to upgrade and why pricing was the way it was - we have now tackled that). Internally, we were able to decrease our workload debt not only in customer service but in operations, development, and sales since we no longer needed to keep track of six different legacy pricing plans. We were then able to contribute our time in more high-value tasks. As we are a small team, <span className="chunky-underline" id="tapcast-billing-underline-color">this was a huge win for us.</span> 
                 <br></br><br></br>
                 As I am writing this (August 2019), we have had minor changes to the original spec such as integrating credit card/billing information from a link to a next step in the upgrade process for a more seamless experience. But overall, we have mostly kept the original flow and pricing model. 
                 </Media>
